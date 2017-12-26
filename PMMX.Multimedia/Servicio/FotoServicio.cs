fad4103f4d9aa247f4398d12e4157f326a7e658a
Enrique Chan Fernandez <enrique.nec@gmail.com>
2017-12-26 14:37:29 -0600

    Cambios Varios

--- PMMX.Multimedia/Servicio/FotoServicio.cs
+++ PMMX.Multimedia/Servicio/FotoServicio.cs
@@ -1,4 +1,5 @@
-﻿using PMMX.Modelo.RespuestaGenerica;
+﻿using PMMX.Infraestructura.Contexto;
+using PMMX.Modelo.RespuestaGenerica;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,18 @@ namespace PMMX.Multimedia.Servicio
 {
     public class FotoServicio
     {
+        private PMMXContext _context;
+
+        public FotoServicio(PMMXContext context)
+        {
+            _context = context;
+        }
+
+        public FotoServicio()
+        {
+            _context = new PMMXContext();
+        }
+
         public IRespuestaServicio<string> SubirFoto() {
             IRespuestaServicio<string> respuesta = new RespuestaServicio<string>();
 
