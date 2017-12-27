39410831f8bb225f211ffb2b4e9a5ebc29421a6b
Enrique Chan Fernandez <enrique.nec@gmail.com>
2017-12-27 16:01:09 -0600

    app.js limpio y con funcion de loading

--- Sitio/PMMX.Sitio/Scripts/app.js
+++ Sitio/PMMX.Sitio/Scripts/app.js
@@ -1,41 +1,29 @@
-﻿'use strict' 
-const express = require('express') 
-const fileSystem = require('fs') 
-const path = require('path') 
-const cfenv = require('cfenv') 
-const ejs = require('ejs') 
-const app = express() 
-//set public as the static resource and view template 
-app.use(express.static(__dirname + '/public')) 
- app.set('views', __dirname + '/public'); 
- app.engine('html', require('ejs').renderFile); 
- //set plist and ipa path 
- const config = require('./config.js') 
- const appPath = '/app/'+ config.APP_NAME +'.ipa' 
- const plistFilePath = path.join(__dirname, '/app/manifest.plist') 
- const ipaFilePath = path.join(__dirname, appPath) 
- 
- 
- app.get('/',function(req,res){ 
-   const downloadUrl = "window.location.href='itms-services://?action=download-manifest&url="+config.WEB_URL + "/app/download'" 
-   res.render('app.html', {downloadUrl: downloadUrl}) 
- }) 
- 
- 
- //response to download action 
- app.get('/app/download',function(req,res){ 
- 	  res.set('Content-Type', 'text/xml plist') 
- 	  res.sendFile(plistFilePath) 
- }) 
- //Set HTTP Header and transfer app stream 
- app.get(appPath,function(req,res){ 
- 		res.set('Content-Type','application/octet-stream ipa') 
- 		res.sendFile(ipaFilePath) 
- }) 
- // get the app environment from Cloud Foundry 
- var appEnv = cfenv.getAppEnv() 
- // start server on the specified port and binding host 
- app.listen(appEnv.port, '0.0.0.0', function() { 
- 	// print a message when the server starts listening 
-   console.log("server starting on " + appEnv.url) 
- }) 
+﻿function divLoading() {
+    var div = "";
+    div += "";
+    div += "<div class=\"box\" id=\"divLoading\">";
+    div += "    <div class=\"box-header with-border\">";
+    div += "        <h3 class=\"box-title\">Title</h3>";
+    div += "        <div class=\"box-tools pull-right\">";
+    div += "            <button type=\"button\" class=\"btn btn-box-tool\" data-widget=\"collapse\" data-toggle=\"tooltip\" title=\"Collapse\">";
+    div += "                <i class=\"fa fa-minus\"></i>";
+    div += "            </button>";
+    div += "            <button type=\"button\" class=\"btn btn-box-tool\" data-widget=\"remove\" data-toggle=\"tooltip\" title=\"Remove\">";
+    div += "                <i class=\"fa fa-times\"></i>";
+    div += "            </button>";
+    div += "        </div>";
+    div += "    </div>";
+    div += "    <div class=\"box-body\">";
+    div += "        <i class=\"fa fa-refresh fa-spin fa-3x fa-fw\" aria-hidden=\"true\"></i>";
+    div += "        <span class=\"sr-only\">Refreshing...</span>Start creating your amazing application!";
+    div += "                </div>";
+    div += "    <!-- /.box-body -->";
+    div += "                <div class=\"box-footer\">";
+    div += "        Footer";
+    div += "                </div>";
+    div += "    <!-- /.box-footer-->";
+    div += "            </div>";
+
+
+    $("#row-content").html(div);             
+}
