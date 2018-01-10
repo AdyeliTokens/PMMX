cc68b1fef47112c76b2455d3247a80a0aceaf2e3
aflores <afloresr07@gmail.com>
2018-01-09 18:26:26 -0600

    StatusVentana HTML

--- Sitio/PMMX.Sitio/Areas/Operaciones/Controllers/StatusVentanaController.cs
+++ Sitio/PMMX.Sitio/Areas/Operaciones/Controllers/StatusVentanaController.cs
@@ -23,35 +23,7 @@ namespace Sitio.Areas.Operaciones.Controllers
         // GET: Operaciones/StatusVentana
         public ActionResult Index()
         {
-            //var statusVentana = db.StatusVentana.ToList();
-            var statusVentana = db.StatusVentana
-            .Select(s => new StatusVentanaView
-            {
-                Id = s.Id,
-                IdVentana = s.IdVentana,
-                IdStatus = s.IdStatus,
-                IdResponsable = s.IdResponsable,
-                Ventana = new VentanaView
-                {
-                    IdEvento = s.Ventana.IdEvento,
-                    PO = s.Ventana.PO,
-                },
-                //Status = new StatusView
-                //{
-                //    Id = s.Status.Id,
-                //    Nombre = s.Status.Nombre,
-                //    NombreCorto = s.Status.NombreCorto,
-                //    Activo = s.Status.Activo
-                //},
-                Responsable = new PersonaView
-                {
-                    Id = s.Responsable.Id,
-                    Nombre = s.Responsable.Nombre,
-                    Apellido1 = s.Responsable.Apellido1,
-                    Apellido2 = s.Responsable.Apellido2
-                }
-            }).ToList();
-
+            var statusVentana = db.StatusVentana.Include(s => s.Responsable).Include(s => s.Ventana).ToList();
             return View(statusVentana);
         }
 
