25f6ff5a0f86147d25f48015921108cf97927990
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-17 15:47:45 -0600

    Crud de CRR y PlanAttainment

--- Sitio/PMMX.Sitio/Areas/Operaciones/Controllers/ObjetivosCRRController.cs
+++ Sitio/PMMX.Sitio/Areas/Operaciones/Controllers/ObjetivosCRRController.cs
@@ -7,111 +7,116 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PMMX.Infraestructura.Contexto;
-using PMMX.Modelo.Entidades;
+using PMMX.Modelo.Entidades.Maquinaria;
 
 namespace Sitio.Areas.Operaciones.Controllers
 {
-    public class MarcasController : Controller
+    public class ObjetivosCRRController : Controller
     {
         private PMMXContext db = new PMMXContext();
 
-        // GET: Operaciones/Marcas
+        // GET: Operaciones/ObjetivosCRR
         public ActionResult Index()
         {
-            return View(db.Marcas.ToList());
+            var objetivosCRR = db.ObjetivosCRR.Include(o => o.WorkCenter);
+            return View(objetivosCRR.ToList());
         }
 
-        // GET: Operaciones/Marcas/Details/5
+        // GET: Operaciones/ObjetivosCRR/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Marca marca = db.Marcas.Find(id);
-            if (marca == null)
+            ObjetivoCRR objetivoCRR = db.ObjetivosCRR.Find(id);
+            if (objetivoCRR == null)
             {
                 return HttpNotFound();
             }
-            return View(marca);
+            return View(objetivoCRR);
         }
 
-        // GET: Operaciones/Marcas/Create
+        // GET: Operaciones/ObjetivosCRR/Create
         public ActionResult Create()
         {
+            ViewBag.IdWorkCenter = new SelectList(db.WorkCenters, "Id", "Nombre");
             return View();
         }
 
-        // POST: Operaciones/Marcas/Create
+        // POST: Operaciones/ObjetivosCRR/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Marca marca)
+        public ActionResult Create([Bind(Include = "Id,IdWorkCenter,Objetivo,FechaInicial")] ObjetivoCRR objetivoCRR)
         {
             if (ModelState.IsValid)
             {
-                db.Marcas.Add(marca);
+                db.ObjetivosCRR.Add(objetivoCRR);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(marca);
+            ViewBag.IdWorkCenter = new SelectList(db.WorkCenters, "Id", "Nombre", objetivoCRR.IdWorkCenter);
+            return View(objetivoCRR);
         }
 
-        // GET: Operaciones/Marcas/Edit/5
+        // GET: Operaciones/ObjetivosCRR/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Marca marca = db.Marcas.Find(id);
-            if (marca == null)
+            ObjetivoCRR objetivoCRR = db.ObjetivosCRR.Find(id);
+            if (objetivoCRR == null)
             {
                 return HttpNotFound();
             }
-            return View(marca);
+            ViewBag.IdWorkCenter = new SelectList(db.WorkCenters, "Id", "Nombre", objetivoCRR.IdWorkCenter);
+            return View(objetivoCRR);
         }
 
-        // POST: Operaciones/Marcas/Edit/5
+        // POST: Operaciones/ObjetivosCRR/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Marca marca)
+        public ActionResult Edit([Bind(Include = "Id,IdWorkCenter,Objetivo,FechaInicial")] ObjetivoCRR objetivoCRR)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(marca).State = EntityState.Modified;
+                db.Entry(objetivoCRR).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(marca);
+            ViewBag.IdWorkCenter = new SelectList(db.WorkCenters, "Id", "Nombre", objetivoCRR.IdWorkCenter);
+            return View(objetivoCRR);
         }
 
-        // GET: Operaciones/Marcas/Delete/5
+        // GET: Operaciones/ObjetivosCRR/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Marca marca = db.Marcas.Find(id);
-            if (marca == null)
+            ObjetivoCRR objetivoCRR = db.ObjetivosCRR.Find(id);
+            if (objetivoCRR == null)
             {
                 return HttpNotFound();
             }
-            return View(marca);
+            return View(objetivoCRR);
         }
 
-        // POST: Operaciones/Marcas/Delete/5
+        // POST: Operaciones/ObjetivosCRR/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Marca marca = db.Marcas.Find(id);
-            db.Marcas.Remove(marca);
+            ObjetivoCRR objetivoCRR = db.ObjetivosCRR.Find(id);
+            db.ObjetivosCRR.Remove(objetivoCRR);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
