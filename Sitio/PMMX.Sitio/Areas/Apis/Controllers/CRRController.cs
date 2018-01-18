0fee19351a53aaf10e5df222fdcbade4c934af24
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-18 09:24:46 -0600

    REfactorizacion de CRRController

--- Sitio/PMMX.Sitio/Areas/Apis/Controllers/CRRController.cs
+++ Sitio/PMMX.Sitio/Areas/Apis/Controllers/CRRController.cs
@@ -1,6 +1,7 @@
 ﻿using PMMX.Infraestructura.Contexto;
 using PMMX.Modelo.Entidades;
 using PMMX.Modelo.Vistas;
+using PMMX.Operaciones.Servicios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,55 +18,9 @@ namespace Sitio.Areas.Apis.Controllers
         [ResponseType(typeof(Desperdicio))]
         public IHttpActionResult GetDesperdiciosPorIdWorkCenter(int IdWorkCenter)
         {
-
-            DateTime diaSeleccionado = DateTime.Now.Date;
-            int delta = DayOfWeek.Monday - diaSeleccionado.DayOfWeek;
-            DateTime monday = diaSeleccionado.AddDays(delta);
-            var primerDiaDelAnio = new DateTime(DateTime.Now.Year, 1, 1);
-
-            var desperdicios = db.Desperdicios
-                .Where(x => (x.IdWorkCenter == IdWorkCenter) && (x.Fecha >= monday && x.Fecha <= diaSeleccionado))
-                .Select(y => new DesperdicioView
-                {
-                    Id = y.Id,
-                    Cantidad = y.Cantidad,
-                    Fecha = y.Fecha,
-                    IdMarca = y.IdMarca,
-                    IdPersona = y.IdPersona,
-                    IdSeccion = y.IdSeccion,
-                    IdWorkCenter = y.IdWorkCenter,
-                    MarcaDelCigarrillo = y.MarcaDelCigarrillo
-                })
-                .ToList();
-
-            var volumenes = db.VolumenesDeProduccion
-                .Where(x => (x.IdWorkCenter == IdWorkCenter) && (x.Fecha >= monday && x.Fecha <= diaSeleccionado))
-                .Select(x => x)
-                .ToList();
-
-            var marcas = desperdicios.Select(x => x.MarcaDelCigarrillo).Distinct();
-
-
-            var objetivos = db.ObjetivosCRR.Select(x => x).Where(x => (x.IdWorkCenter == IdWorkCenter) && (x.FechaInicial >= primerDiaDelAnio)).ToList();
-
-            IList<CRRView> crr = new List<CRRView>();
-            for (int i = delta; i <= (6 + delta); i++)
-            {
-                Double crrTotal = 0;
-                foreach (var item in marcas)
-                {
-                    Double crrTotalPorMarca = desperdicios.Where(x => (x.Fecha.Date == diaSeleccionado.AddDays(i).Date) && (x.IdMarca == item.Id)).Sum(o => o.Cantidad);
-                    Double volumen = volumenes.Where(v => (v.IdMarca == item.Id) && (v.Fecha.Date == diaSeleccionado.AddDays(i).Date)).Sum(o => o.Cantidad);
-
-                    crrTotal = crrTotalPorMarca / volumen;
-                }
-                //Double crrTotal = desperdicios.Where(x => x.Fecha.Date == diaSeleccionado.AddDays(i).Date).Sum(o => o.Cantidad);
-                Double objetivo = objetivos.Where(x => x.FechaInicial <= diaSeleccionado.AddDays(i).Date).OrderByDescending(x => x.FechaInicial).Select(x => x.Objetivo).FirstOrDefault();
-
-                crr.Add(new CRRView { Fecha = diaSeleccionado.AddDays(i), CRR_Total = crrTotal, Objetivo = objetivo });
-            }
-
-            return Ok(crr);
+            DesperdicioServicio servicio = new DesperdicioServicio(db);
+            var respuesta = servicio.GetCRRPorWorkCenterPorSemanaActual(IdWorkCenter);
+            return Ok(respuesta);
         }
 
 
