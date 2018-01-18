6fa401392865628274baf623f371c4824684412d
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-18 09:24:23 -0600

    Creacion de PlanAttainment View

---
+++ PMMX.Modelo/Vistas/PlanAttainmentView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMMX.Modelo.Vistas
+{
+    public class PlanAttainmentView
+    {
+        public Double Plan_Attainment_Total { get; set; }
+        public Double Objetivo { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
