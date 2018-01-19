2bd7096a7f68b811aca2ee7bf3638fe688ecef66
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-19 15:57:28 -0600

    Creacion de metodos Post Put y Delete

--- Operaciones/PMMX.Operaciones/Servicios/ActividadEnParoServicio.cs
+++ Operaciones/PMMX.Operaciones/Servicios/ActividadEnParoServicio.cs
@@ -3,6 +3,8 @@ using PMMX.Modelo.Entidades.Paros;
 using PMMX.Modelo.RespuestaGenerica;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,9 @@ namespace PMMX.Operaciones.Servicios
         {
             this._context = _context;
         }
+
+
+        #region Get
 
         public RespuestaServicio<IQueryable<ActividadEnParo>> GetActividadesEnParo()
         {
@@ -50,5 +55,89 @@ namespace PMMX.Operaciones.Servicios
 
             return respuesta;
         }
+
+        public bool ActividadEnParoExists(int id)
+        {
+            return _context.ActividadEnParos.Count(e => e.Id == id) > 0;
+        }
+
+        #endregion
+
+        #region Put
+
+        public RespuestaServicio<ActividadEnParo> PutActividadEnParo(int id, ActividadEnParo actividadEnParo)
+        {
+            RespuestaServicio<ActividadEnParo> respuesta = new RespuestaServicio<ActividadEnParo>();
+            if (id != actividadEnParo.Id)
+            {
+                respuesta.Mensaje = "El identificador mandado no coincide con el de la Actividad en paro.";
+            }
+
+            _context.Entry(actividadEnParo).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+                respuesta.Respuesta = actividadEnParo;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActividadEnParoExists(id))
+                {
+                    respuesta.Mensaje = "La Actividad en el paro no Existe";
+                }
+            }
+
+            return respuesta;
+        }
+        #endregion
+
+        #region Post
+        public RespuestaServicio<ActividadEnParo> PostActividadEnParo(ActividadEnParo actividadEnParo)
+        {
+            RespuestaServicio<ActividadEnParo> respuesta = new RespuestaServicio<ActividadEnParo>();
+            try
+            {
+                _context.ActividadEnParos.Add(actividadEnParo);
+                _context.SaveChanges();
+
+                respuesta.Respuesta = actividadEnParo;
+            }
+            catch (Exception e)
+            {
+
+                respuesta.Mensaje = e.Message;
+            }
+
+
+            return respuesta;
+        }
+        #endregion
+
+        #region Delete
+        public RespuestaServicio<ActividadEnParo> DeleteActividadEnParo(int id)
+        {
+            RespuestaServicio<ActividadEnParo> respuesta = new RespuestaServicio<ActividadEnParo>();
+            ActividadEnParo actividadEnParo = _context.ActividadEnParos.Find(id);
+            if (actividadEnParo == null)
+            {
+                respuesta.Mensaje = "La Actividad No puede ser Borrada porque no existe";
+            }
+            try
+            {
+                _context.ActividadEnParos.Remove(actividadEnParo);
+                _context.SaveChanges();
+                respuesta.Respuesta = actividadEnParo;
+            }
+            catch (Exception e)
+            {
+                respuesta.Mensaje = e.Message;
+            }
+
+
+            return respuesta;
+        }
+        #endregion
+
     }
 }
