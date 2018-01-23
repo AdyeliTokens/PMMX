5b81e3ed0059120a8fedd848b58d83f1f4954468
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-23 08:55:37 -0600

    Creacion de Vistas para Actividades en defecto y actividades En paro

---
+++ PMMX.Modelo/Vistas/ActividadEnParoView.cs
@@ -0,0 +1,25 @@
+﻿using PMMX.Modelo.Entidades;
+using PMMX.Modelo.Entidades.Paros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMMX.Modelo.Vistas
+{
+    public class ActividadEnParoView
+    {
+        public int Id { get; set; }
+        public int IdPersona { get; set; }
+        public int IdParo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+
+
+        public ParoView Paro { get; set; }
+        public PersonaView Ejecutante { get; set; }
+    }
+}
