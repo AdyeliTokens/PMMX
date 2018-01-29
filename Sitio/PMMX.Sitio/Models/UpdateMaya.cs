658fe78ee5799da017355d6372978434ef14edef
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-29 16:08:04 -0600

    Creacion de UpdateMaya

---
+++ Sitio/PMMX.Sitio/Models/UpdateMaya.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitio.Models
+{
+    public class UpdateMaya
+    {
+        public Double NewVersion { get; set; }
+        public String url { get; set; }
+        public String releaseNotes { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
