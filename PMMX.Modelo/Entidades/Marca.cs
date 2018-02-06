93fb49af104ed46077125635a5bc330ce835aa12
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-02-06 11:49:00 -0600

    Cambio de ¿Entidad Marca

--- PMMX.Modelo/Entidades/Marca.cs
+++ PMMX.Modelo/Entidades/Marca.cs
@@ -10,15 +10,20 @@ namespace PMMX.Modelo.Entidades
     public class Marca
     {
         public int Id { get; set; }
-
         [StringLength(250)]
-        public string Nombre { get; set; }
-
+        public string Descripcion { get; set; }
         [StringLength(250)]
-        public string Codigo { get; set; }
-        public Boolean Activo { get; set; }
+        public string Codigo_FA { get; set; }
+        [StringLength(250)]
+        public string Codigo_Cigarrillo { get; set; }
         public Double? PesoPorCigarrillo { get; set; }
+        public Double? PesoTabacco { get; set; }
+        public Boolean Activo { get; set; }
 
+        public DateTime FechaDeAlta { get; set; }
+        public int IdPersonaQueDioDeAlta { get; set; }
+
+        public Persona PersonaQueDioDeAlta { get; set; }
         public ICollection<Desperdicio> Desperdicios { get; set; }
         public ICollection<VolumenDeProduccion> VolumenesProducidos { get; set; }
     }
