4ae483bf0a54efa98fc314d8c66c1f9151191e41
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-02-07 13:36:25 -0600

    MarcaServicioTest

---
+++ Operaciones/PMMX.OperacionesTests/Servicios/MarcaServicioTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMMX.Infraestructura.Contexto;
+using PMMX.Operaciones.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMMX.Operaciones.Servicios.Tests
+{
+    [TestClass()]
+    public class MarcaServicioTests
+    {
+
+        private readonly MarcaServicio _servicio;
+        private readonly PMMXContext _context;
+
+
+        public MarcaServicioTests()
+        {
+
+            _context = new PMMXContext();
+            _servicio = new MarcaServicio(_context);
+
+        }
+
+
+
+        [TestMethod()]
+        public void GetMarcasTest()
+        {
+            var respuesta = _servicio.GetMarcas();
+            Assert.IsTrue(respuesta.EjecucionCorrecta);
+        }
+    }
+}
