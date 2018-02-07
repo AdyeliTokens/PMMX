fd94cc86f6d96bfe718e0e34e87922cc91754ade
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-02-07 12:40:24 -0600

    Creacion de no conformidades Servicio

---
+++ Operaciones/PMMX.Operaciones/Servicios/NoConformidadServicio.cs
@@ -0,0 +1,59 @@
+﻿using PMMX.Infraestructura.Contexto;
+using PMMX.Modelo.Entidades;
+using PMMX.Modelo.RespuestaGenerica;
+using System;
+using System.Linq;
+using System.Data.Entity;
+
+namespace PMMX.Operaciones.Servicios
+{
+    public class NoConformidadServicio
+    {
+        private PMMXContext _context;
+
+        public NoConformidadServicio(PMMXContext context)
+        {
+            _context = context;
+        }
+
+        public NoConformidadServicio()
+        {
+            _context = new PMMXContext();
+        }
+
+
+        #region Gets
+
+        public RespuestaServicio<IQueryable<NoConformidad>> GetNoConformidades()
+        {
+            RespuestaServicio<IQueryable<NoConformidad>> respuesta = new RespuestaServicio<IQueryable<NoConformidad>>();
+            try
+            {
+                var noconformidades = _context.NoConformidades.Include(n => n.Persona).Include(n => n.Seccion).Include(n => n.WorkCenter);
+                respuesta.Respuesta = noconformidades;
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return respuesta;
+        }
+
+        public RespuestaServicio<NoConformidad> GetNoConformidad(int id)
+        {
+            RespuestaServicio<NoConformidad> respuesta = new RespuestaServicio<NoConformidad>();
+            try
+            {
+                var noconformidad = _context.NoConformidades.Include(n => n.Persona).Include(n => n.Seccion).Include(n => n.WorkCenter).Where(n => n.Id == id).FirstOrDefault(); ;
+                respuesta.Respuesta = noconformidad;
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return respuesta;
+        }
+
+        #endregion
+    }
+}
