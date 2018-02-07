5fbcd9f7d36bc926b6a2153525d5adbcf7df62be
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-02-07 12:40:45 -0600

    Incorporacion de no conformidadesServicioTest

---
+++ Operaciones/PMMX.OperacionesTests/Servicios/NoConformidadServicioTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMMX.Infraestructura.Contexto;
+using PMMX.Operaciones.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMMX.Operaciones.Servicios.Tests
+{
+    [TestClass()]
+    public class NoConformidadServicioTests
+    {
+
+        private readonly NoConformidadServicio _servicio;
+        private readonly PMMXContext _context;
+
+
+        public NoConformidadServicioTests()
+        {
+
+            _context = new PMMXContext();
+            _servicio = new NoConformidadServicio(_context);
+
+        }
+
+
+
+        [TestMethod()]
+        public void GetNoConformidadesTest()
+        {
+            var respuesta = _servicio.GetNoConformidades();
+            Assert.IsTrue(respuesta.EjecucionCorrecta);
+        }
+
+        [TestMethod()]
+        [DataRow(12)]
+        [DataRow(6)]
+        [DataRow(3)]
+        public void GetNoConformidTest(int id)
+        {
+            var respuesta = _servicio.GetNoConformidad(id);
+            Assert.IsTrue(respuesta.EjecucionCorrecta);
+        }
+    }
+}
