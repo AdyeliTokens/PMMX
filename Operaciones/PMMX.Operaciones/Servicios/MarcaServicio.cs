6534b95c444e503f8daa1e1bae474b36ec8d88e6
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-02-07 13:36:01 -0600

    Marca Servicio

---
+++ Operaciones/PMMX.Operaciones/Servicios/MarcaServicio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMMX.Infraestructura.Contexto;
+using PMMX.Modelo.RespuestaGenerica;
+using PMMX.Modelo.Entidades;
+
+namespace PMMX.Operaciones.Servicios
+{
+    public class MarcaServicio
+    {
+        private PMMXContext _context;
+
+        public MarcaServicio()
+        {
+            _context = new PMMXContext();
+        }
+
+        public MarcaServicio(PMMXContext db)
+        {
+            _context = db;
+        }
+
+        public RespuestaServicio<IQueryable<Marca>> GetMarcas()
+        {
+            RespuestaServicio<IQueryable<Marca>> respuesta = new RespuestaServicio<IQueryable<Marca>>();
+            try
+            {
+                var marcas = _context.Marcas;
+                respuesta.Respuesta = marcas;
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return respuesta;
+        }
+    }
+}
