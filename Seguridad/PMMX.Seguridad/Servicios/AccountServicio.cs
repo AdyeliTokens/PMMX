f8eb1abda54b2fc80827b2d3c8ea1fb20813afb0
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-02-13 11:34:31 -0600

    Actualizacion para login con aspnet user

--- Seguridad/PMMX.Seguridad/Servicios/AccountServicio.cs
+++ Seguridad/PMMX.Seguridad/Servicios/AccountServicio.cs
@@ -52,7 +52,33 @@ namespace PMMX.Seguridad.Servicios
             }
 
         }
-        
+
+        public RespuestaServicio<UserView> ExternalLogin(LoginModel login)
+        {
+            RespuestaServicio<UserView> respuesta = new RespuestaServicio<UserView>();
+            respuesta = GetUserExternal(login);
+            if (respuesta.EjecucionCorrecta)
+            {
+                if (login.Llave != null || login.Llave != "")
+                {
+                    DispositivoServicio dispositivoServicio = new DispositivoServicio();
+                    RespuestaServicio<DispositivoView> dispositivoRespuesta = new RespuestaServicio<DispositivoView>();
+                    dispositivoRespuesta = dispositivoServicio.DispositivoActualByPersona(respuesta.Respuesta.Persona.Id, login.Llave);
+                    if (dispositivoRespuesta.EjecucionCorrecta == false)
+                    {
+                        return respuesta;
+                    }
+                }
+
+                return respuesta;
+            }
+            else
+            {
+                return respuesta;
+            }
+
+        }
+
         public RespuestaServicio<UserView> GetUser(LoginModel login)
         {
             RespuestaServicio<UserView> respuesta = new RespuestaServicio<UserView>();
@@ -97,6 +123,74 @@ namespace PMMX.Seguridad.Servicios
             if (user != null)
             {
                 if (user.Entornos.Count() == 1) {
+                    user.Entorno = user.Entornos.FirstOrDefault();
+                    user.IdEntorno = user.Entornos.FirstOrDefault().Id;
+                }
+
+                if (user.Persona == null)
+                {
+                    respuesta.Mensaje = "Usuario no esta ligado a una persona";
+                    return respuesta;
+                }
+                else
+                {
+                    respuesta.Respuesta = user;
+                }
+            }
+            else
+            {
+                respuesta.Mensaje = "Usuario no encontrado";
+                return respuesta;
+            }
+
+            return respuesta;
+        }
+
+        public RespuestaServicio<UserView> GetUserExternal(LoginModel login)
+        {
+            RespuestaServicio<UserView> respuesta = new RespuestaServicio<UserView>();
+            var user = _context.Users
+                .Where(x => x.Email == login.Email)
+                .Select(u => new UserView
+                {
+                    Id = u.Id,
+                    Email = u.Email,
+                    IdPersona = u.IdPersona,
+                    Password = u.Password,
+                    Activo = u.Activo,
+                    UserName = u.UserName,
+                    Entornos = u.Entornos.Select(e => new EntornoView
+                    {
+                        Id = e.Id,
+                        Nombre = e.Nombre,
+                        Activo = e.Activo
+                    }).Where(o => o.Activo == true).ToList(),
+                    Persona = new PersonaView
+                    {
+                        Id = u.Persona.Id,
+                        Nombre = u.Persona.Nombre,
+                        Apellido1 = u.Persona.Apellido1,
+                        Apellido2 = u.Persona.Apellido2,
+                        IdPuesto = u.Persona.IdPuesto,
+                        Activo = u.Persona.Activo,
+                        Puesto = new PuestoView
+                        {
+                            Id = u.Persona.Puesto.Id,
+                            Nombre = u.Persona.Puesto.Nombre,
+                            Activo = u.Persona.Puesto.Activo
+                        }
+                    }
+                })
+                .FirstOrDefault();
+
+
+
+
+
+            if (user != null)
+            {
+                if (user.Entornos.Count() == 1)
+                {
                     user.Entorno = user.Entornos.FirstOrDefault();
                     user.IdEntorno = user.Entornos.FirstOrDefault().Id;
                 }
