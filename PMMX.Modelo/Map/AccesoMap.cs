9e62e2e58e83893a82ae1d2eaf2000892f028944
aflores <afloresr07@gmail.com>
2018-08-13 12:47:38 -0500

    Acceso Created

---
+++ PMMX.Modelo/Map/AccesoMap.cs
@@ -0,0 +1,47 @@
+﻿using PMMX.Modelo.Entidades.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMMX.Modelo.Entidades;
+using System.Data.Entity.ModelConfiguration;
+
+
+namespace PMMX.Modelo.Map
+{
+    public class AccesoMap : EntityTypeConfiguration<Acceso>
+    {
+        public AccesoMap()
+        {
+            #region Properties
+            ToTable("Accesos");
+            HasKey(c => c.Id);
+            Property(c => c.Id).HasColumnName("Id");
+            Property(c => c.Area).HasColumnName("Area");
+            Property(c => c.Menu).HasColumnName("Menu");
+            Property(c => c.SubMenu).HasColumnName("SubMenu");
+            Property(c => c.Programa).HasColumnName("Programa");
+            Property(c => c.Ruta).HasColumnName("Ruta");
+            Property(c => c.Activo).HasColumnName("Activo");
+
+            #endregion
+
+            #region HasRequired
+            #endregion
+
+            #region HasMany
+            HasMany(c => c.Personas).WithMany(x => x.Accesos).Map(cs =>
+            {
+                cs.MapLeftKey("IdAcceso");
+                cs.MapRightKey("IdPersona");
+                cs.ToTable("AccesoPersona");
+            });
+            #endregion
+
+            #region HasOptional
+            #endregion
+        }
+
+    }
+}
---
+++ Sitio/PMMX.Sitio/Areas/Seguridad/Controllers/AccesoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PMMX.Infraestructura.Contexto;
+using PMMX.Modelo.Entidades.Seguridad;
+
+namespace Sitio.Areas.Seguridad.Controllers
+{
+    public class AccesoController : Controller
+    {
+        private PMMXContext db = new PMMXContext();
+
+        // GET: Seguridad/Acceso
+        public ActionResult Index()
+        {
+            return View(db.Accesos.ToList());
+        }
+
+        // GET: Seguridad/Acceso/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acceso acceso = db.Accesos.Find(id);
+            if (acceso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acceso);
+        }
+
+        // GET: Seguridad/Acceso/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Seguridad/Acceso/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Area,Menu,SubMenu,Programa,Ruta,Activo")] Acceso acceso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accesos.Add(acceso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(acceso);
+        }
+
+        // GET: Seguridad/Acceso/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acceso acceso = db.Accesos.Find(id);
+            if (acceso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acceso);
+        }
+
+        // POST: Seguridad/Acceso/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Area,Menu,SubMenu,Programa,Ruta,Activo")] Acceso acceso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(acceso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(acceso);
+        }
+
+        // GET: Seguridad/Acceso/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acceso acceso = db.Accesos.Find(id);
+            if (acceso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acceso);
+        }
+
+        // POST: Seguridad/Acceso/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Acceso acceso = db.Accesos.Find(id);
+            db.Accesos.Remove(acceso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
